edb1d75b6c0a7a7b40aca896b01e58cf132929d6
shoaibMalik <shoaib.malik1995.sm@gmail.com>
2024-07-08 10:30:39 +0300

    commect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to the server ");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("server is run corectly ");
 });
